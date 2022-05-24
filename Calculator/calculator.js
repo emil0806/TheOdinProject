168639527499455581c58abc06a480862520da90
emil0806 <emil6008@gmail.com>
2022-05-24 16:55:56 +0200

    .

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,1 +1,13 @@
+function clearScreen() {
+    document.getElementById("display").value = "";
+}
 
+function display(value) {
+    document.getElementById("display").value += value;
+}
+
+function calculate() {
+    let p = document.getElementById("display").value;
+    let q = eval(p);
+    document.getElementById("display").value = q;
+}
