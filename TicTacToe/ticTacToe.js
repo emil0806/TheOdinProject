7c26b8da4898c93df3d144d8d075b2bfcebc71c3
emil0806 <emil6008@gmail.com>
2022-07-19 22:29:57 +0200

    Adding reset game and few other settings

--- TicTacToe/ticTacToe.js
+++ TicTacToe/ticTacToe.js
@@ -2,6 +2,8 @@
 const gameBoard = (() =>  {
     // Get container from HTML
     const container = document.getElementById('container');
+    const containBtn = document.getElementById('divBtn');
+    let congrats = document.createElement('h3')
 
     // Create board array to store marks
     let board = [];
@@ -13,6 +15,15 @@ const gameBoard = (() =>  {
         boardCell.id = i;
         container.appendChild(boardCell).className = "boardCell"
     }
+
+    // Create and append button for reset game
+    let button = document.createElement('button')
+    button.id = 'resetGame'
+    button.innerHTML = 'Restart game!'
+    containBtn.appendChild(button).className = "button"
+    button.addEventListener('click', () => {
+        resetGame()
+    })
 
     // Player factory, used to create objects of players
     const playerFactory = (name, mark, turn) => {
@@ -35,16 +46,16 @@ const gameBoard = (() =>  {
         box.forEach( box => {
            box.addEventListener('click', e => {
             // Setting conditions for both players, updating board array, turn, text, and calling checkWinner function
-            if (player1.turn == true && e.target.textContent == '') {
+            if (player1.turn == true && e.target.textContent == '' && winner == null || undefined) {
                 board[e.target.id] = player1.mark
-                box.textContent = 'X'
+                box.textContent = board[e.target.id]
                 player1.turn = false;
                 player2.turn = true;
                 checkWinner(player1.name, player1.mark)
             }
-            else if (player2.turn == true && e.target.textContent == '') {
+            else if (player2.turn == true && e.target.textContent == '' && winner == null || undefined) {
                 board[e.target.id] = player2.mark
-                box.textContent = 'O';
+                box.textContent = board[e.target.id];
                 player1.turn = true;
                 player2.turn = false;
                 checkWinner(player2.name, player2.mark)
@@ -59,16 +70,22 @@ const gameBoard = (() =>  {
     // Function that checkes if anybody won the game, or it is a tie
     function checkWinner(name, symbol) {
         turns++;
-        if  (board[0] === symbol && board[1] === symbol && board[2] === symbol
-            || board[3] === symbol && board[4] === symbol && board[5] === symbol
-            || board[6] === symbol && board[7] === symbol && board[8] === symbol
-            || board[0] === symbol && board[3] === symbol && board[6] === symbol
-            || board[1] === symbol && board[4] === symbol && board[7] === symbol
-            || board[2] === symbol && board[5] === symbol && board[8] === symbol
-            || board[0] === symbol && board[4] === symbol && board[8] === symbol
-            || board[2] === symbol && board[4] === symbol && board[6] === symbol)
-            
+        if (board[0] === symbol && board[1] === symbol && board[2] === symbol
+        || board[3] === symbol && board[4] === symbol && board[5] === symbol
+        || board[6] === symbol && board[7] === symbol && board[8] === symbol
+        || board[0] === symbol && board[3] === symbol && board[6] === symbol
+        || board[1] === symbol && board[4] === symbol && board[7] === symbol
+        || board[2] === symbol && board[5] === symbol && board[8] === symbol
+        || board[0] === symbol && board[4] === symbol && board[8] === symbol
+        || board[2] === symbol && board[4] === symbol && board[6] === symbol) {
+                
             console.log(`${name} wins`)
+            console.log(board)
+            winner = name;
+            congrats.textContent = `Congrats ${name}. You won!`
+            containBtn.appendChild(congrats).className = 'congrats'
+        }
+            
         else if (turns == 9 && winner == null && winner == undefined) {
             console.log('It is a tie!')
         }
@@ -77,6 +94,21 @@ const gameBoard = (() =>  {
             
         }
             
+    }
+
+    function resetGame() {
+        board = ["", "", "", "", "", "", "", "", ""];
+        turns = 0;
+        player1.turn = true;
+        player2.turn = false;
+        winner = null;
+
+        let elems = document.querySelectorAll('.boardCell')
+        for (let i = 0; i < elems.length; i++) {
+            elems[i].textContent = "";
+        } 
+        congrats.textContent = "";
+        console.log(board)
     }
 
 })();
