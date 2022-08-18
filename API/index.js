cae98d5e27ee2956bb8cf478aa16d3d09f9c8594
emil0806 <emil6008@gmail.com>
2022-08-18 14:40:32 +0200

    Using async/await instead

--- API/index.js
+++ API/index.js
@@ -5,37 +5,30 @@ const errorContainer = document.getElementById("errorContainer");
 
 let anotherGif = "";
 
-function getImage(anotherGif) {
-  fetch(
+async function getImage(anotherGif) {
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=WSHEqNXhd3wXH9mcqeJOYNceifUIQwLI&s=${anotherGif}`,
     { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    });
+  );
+  const imageData = await response.json();
+  img.src = imageData.data.images.original.url;
 }
 
-function searchImg(searchText) {
-  fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=WSHEqNXhd3wXH9mcqeJOYNceifUIQwLI&s=${searchText}`,
-    { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    })
-    .catch(function (error) {
-      let errorText = document.createElement("h3");
-      errorText.textContent = "No GIF with that name. Try something else!";
-      errorText.id = "errorText";
+async function searchImg(searchText) {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=WSHEqNXhd3wXH9mcqeJOYNceifUIQwLI&s=${searchText}`,
+      { mode: "cors" }
+    );
+    const imageSearch = await response.json();
+    img.src = imageSearch.data.images.original.url;
+  } catch (error) {
+    let errorText = document.createElement("h3");
+    errorText.textContent = "No GIF with that name. Try something else!";
+    errorText.id = "errorText";
 
-      errorContainer.appendChild(errorText);
-    });
+    errorContainer.appendChild(errorText);
+  }
 }
 changeGif.addEventListener("click", () => {
   errorContainer.innerHTML = "";
